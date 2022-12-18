db0271451cb8c385bda0f5025f9764d3b79d95b7
James Bennett <jwbennet@gmail.com>
2022-12-18 00:40:57 -0500

    feat: Enables remote desktop on the target machine

--- boxstarter.ps1
+++ boxstarter.ps1
@@ -1,8 +1,12 @@
 # Description: Boxstarter script for bootstrapping my developer workstation
 
+Enable-RemoteDesktop
+
 choco install -y chezmoi
 
+# This step accepts the Microsoft Store agreement so we can install packages from there
 winget list --accept-source-agreements --accept-package-agreements
+
 winget install -e --id 7zip.7zip
 winget install -e --id Docker.DockerDesktop
 winget install -e --id GitHub.GitHubDesktop
