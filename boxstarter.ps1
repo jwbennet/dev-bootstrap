9d6713c0334fe56b28cc4ee4fb6f4bed5a696c1a
James Bennett <jwbennet@gmail.com>
2022-12-19 09:49:37 -0500

    feat: Reorder Boxstarter steps
    This moves the WSL setup to earlier in the setup process in an attempt to have it complete before the Docker Desktop installation.

--- boxstarter.ps1
+++ boxstarter.ps1
@@ -7,22 +7,26 @@ choco install -y chezmoi
 # This step accepts the Microsoft Store agreement so we can install packages from there
 winget list --accept-source-agreements --accept-package-agreements
 
+# Windows Sub-system for Linux
+## Download the Linux kernel update package
+choco install -y Microsoft-Windows-Subsystem-Linux -source windowsfeatures
+choco install -y VirtualMachinePlatform -source windowsfeatures
+wsl --set-default-version 2
+choco install wsl2 --params "/Version:2 /Retry:true"
+wsl --install --distribution Ubuntu
+wsl --export Ubuntu "$env:TEMP\ubuntu.tar.gz"
+wsl --import dev "$env:TEMP\wsl-dev" "$env:TEMP\ubuntu.tar.gz"
+wsl --set-default dev
+wsl --unregister Ubuntu
+wsl useradd -m -G sudo -s /bin/bash "jwbennet"
+
+# Install Applications
 winget install -e --id 7zip.7zip
 winget install -e --id Docker.DockerDesktop
 winget install -e --id GitHub.GitHubDesktop
 winget install -e --id Google.Chrome
 winget install -e --id Postman.Postman
 winget install -e --id Microsoft.WindowsTerminal
-
-# Windows Sub-system for Linux
-## Download the Linux kernel update package
-choco install wsl2 --params "/Version:2 /Retry:true"
-wsl --install --distribution Ubuntu --no-launch
-wsl --set-default-version 2
-wsl --export Ubuntu "$env:TEMP\ubuntu.tar.gz"
-wsl --import dev "$env:TEMP\wsl-dev" "$env:TEMP\ubuntu.tar.gz"
-wsl --set-default dev
-wsl --unregister Ubuntu
 
 #Enable-MicrosoftUpdate
 #Install-WindowsUpdate -acceptEula
