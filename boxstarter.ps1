ae511bfe5dc5f108f6657acffa5e4e3ec5fad087
James Bennett <jwbennet@gmail.com>
2022-12-23 16:50:20 -0500

    fix: Do not disable windows arrangement
    Found that the Snap Assist configuration only seems to take effect on reboot so I think this was overly aggressive. I want the configuration on for typical window snapping.

--- boxstarter.ps1
+++ boxstarter.ps1
@@ -154,8 +154,6 @@ reg.exe add "HKCU\Software\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}\
 ## Disable Snap Assist
 Set-ItemProperty -Path HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced -Name "EnableSnapAssistFlyout" -Value 0
 Set-ItemProperty -Path HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced -Name "SnapAssist" -Value 0
-Set-ItemProperty -Path "HKCU:\Control Panel\Desktop" -Name "WindowArrangementActive" -Value 0
-
 
 # Disable various unused taskbar features in Windows 11
 Set-ItemProperty -Path HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced -Name ShowTaskViewButton -Value 0
